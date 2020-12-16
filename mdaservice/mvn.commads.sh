f56749c46b32a7c8db3a8297c48ba563c8e067a2
EZ <ezgoesit247@gmail.com>
2020-12-16 16:04:22 -0600

    Fixed bad login

--- mdaservice/mvn.commads.sh
+++ mdaservice/mvn.commads.sh
@@ -1,30 +1,32 @@
 #mdaservice
 
-SERVICE_TEST_ENVIRONMENT=DV1
-mvn clean package -DSERVICE_TEST_ENVIRONMENT=${SERVICE_TEST_ENVIRONMENT}
+MDA_SERVICE_VERSION=1.1.4-SNAPSHOT \
+  && TEST_ENVIRONMENT=DV1 \
+  && TEST_RUN_ENVIRONMENT=QA1 \
+  && INT_ENVIRONMENT=INT \
+  && RUN_ENVIRONMENT=INT
 
+# package
 # clean, then package skip tests
-mvn clean && if [ ! -d target ]; then
-    echo mvn clean failed; fi \
-  && mvn package -DskipTests
-
 # clean & run
-ENV=QA1
-mvn clean && if [ ! -d target ]; then
-  mvn spring-boot:run -Dspring-boot.run.profiles=${ENV}; else
-  echo mvn clean failed; fi
+mvn clean package -DSERVICE_TEST_ENVIRONMENT=${TEST_ENVIRONMENT} \
+\
+&& mvn clean && if [ -d target ]; then
+    echo mvn clean failed; else
+    mvn package -DskipTests; fi \
+\
+&& if [ -f target/mdaservice-${MDA_SERVICE_VERSION}.jar ]; then
+  mvn spring-boot:run -Dspring-boot.run.profiles=${TEST_RUN_ENVIRONMENT}; else
+  echo packaging jar failed; fi
 
 
 # clean, then package skip tests
-SERVICE_TEST_ENVIRONMENT=DV1
 mvn clean && if [ ! -d target ]; then
   mvn package \
-    -DSERVICE_TEST_ENVIRONMENT=${SERVICE_TEST_ENVIRONMENT} \
-    -DskipTests; else
+    -DSERVICE_TEST_ENVIRONMENT=${INT_ENVIRONMENT}; else
   echo mvn clean failed; fi
 
 # run the jar
-MDA_SERVICE_VERSION=1.1.4-SNAPSHOT
 java -jar \
-   -Dspring.profiles.active=QA1 \
+   -Dspring.profiles.active=${RUN_ENVIRONMENT} \
    target/mdaservice-${MDA_SERVICE_VERSION}.jar
