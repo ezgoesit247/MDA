5577af16c0fcbdc41f84d668fc13bc3e4ee13729
EZ <ezgoesit247@gmail.com>
2021-02-07 20:27:41 +0000

    functionized maven commands

--- mvn.commads.sh
+++ mvn.commads.sh
@@ -1,15 +1,16 @@
 #mdaservice
 
 MDA_SERVICE_VERSION=1.1.4-SNAPSHOT \
-  && TEST_ENVIRONMENT=DV1 \
+  && TEST_INT_ENVIRONMENT=DV1 \
   && TEST_RUN_ENVIRONMENT=QA1 \
   && INT_ENVIRONMENT=INT \
   && RUN_ENVIRONMENT=INT
 
+function do_test_integration {
 # package
 # clean, then package skip tests
 # clean & run
-mvn clean package -DSERVICE_TEST_ENVIRONMENT=${TEST_ENVIRONMENT} \
+mvn clean package -DSERVICE_TEST_ENVIRONMENT=${TEST_INT_ENVIRONMENT} \
 \
 && mvn clean && if [ -d target ]; then
     echo mvn clean failed; else
@@ -19,15 +20,19 @@ mvn clean package -DSERVICE_TEST_ENVIRONMENT=${TEST_ENVIRONMENT} \
   mvn spring-boot:run -Dspring-boot.run.profiles=${TEST_RUN_ENVIRONMENT}; else
   echo packaging jar failed; fi && popd >/dev/null || popd >/dev/null \
 && find . -name mdaservice-${MDA_SERVICE_VERSION}.jar
+}
 
-
+function do_integration {
 # clean, then package skip tests
 mvn clean && if [ ! -d target ]; then
   mvn package \
     -DSERVICE_TEST_ENVIRONMENT=${INT_ENVIRONMENT}; else
   echo mvn clean failed; fi
+}
 
+function do_run {
 # run the jar
 java -jar \
    -Dspring.profiles.active=${RUN_ENVIRONMENT} \
    mdaservice/target/mdaservice-${MDA_SERVICE_VERSION}.jar
+}
